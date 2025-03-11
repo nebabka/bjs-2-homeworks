a73b4c818099850280a35aa4ed2b15e3af61fab3
nebabka <nastyfedinadura@gmail.com>
2025-03-11 17:07:50 +0300

    Задание №1 готово (5) 3

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,29 +1,29 @@
 class PrintEditionItem {
- constructor (name, releaseDate, pagesCount){
-    this.name = name;
-    this.releaseDate = releaseDate;
-    this.pagesCount = pagesCount;
-    this._state = 100;
-    this.type = null;
-  }
-
-  fix() {
-    if (this._state > 0 && this._state < 100) { 
-      this._state = Math.min(100, this._state * 1.5); 
-    }
-  }
-
-  get state() {
-    return this._state;
-  }
-
-  set state(newState) {
-    if (newState < 0) {
-      this._state = 0;
-    } else if (newState > 100) {
+    constructor(name, releaseDate, pagesCount) {
+      this.name = name;
+      this.releaseDate = releaseDate;
+      this.pagesCount = pagesCount;
       this._state = 100;
-    } else {
-      this._state = newState;
+      this.type = null;
+    }
+  
+    fix() {
+      if (this._state > 0 && this._state < 100) {
+        this._state = Math.min(100, this._state * 1.5);
+      }
+    }
+  
+    get state() {
+      return this._state;
+    }
+  
+    set state(newState) {
+      if (newState < 0) {
+        this._state = 0;
+      } else if (newState > 100) {
+        this._state = 100;
+      } else {
+        this._state = newState;
+      }
     }
   }
-}
