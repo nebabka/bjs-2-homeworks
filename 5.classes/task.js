276f9aa15d8cc262923efde1607c4ef647a42d61
nebabka <nastyfedinadura@gmail.com>
2025-03-11 17:22:38 +0300

    Задание №1 готово (5) 8

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -27,3 +27,39 @@ class PrintEditionItem {
         return this._state;
       }
   }
+
+  class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = "magazine";
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = "book";
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "detective";
+    }
+}
+
+class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "fantastic";
+    }
+}
+
+class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "novel";
+    }
+}
