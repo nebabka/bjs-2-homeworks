873fd99678edd41f04dbd237b3d4c5a3d0ce1d94
nebabka <nastyfedinadura@gmail.com>
2025-03-14 14:51:02 +0300

    Задания готовы (6)

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,4 +1,5 @@
-﻿// Задание №1
+﻿// Задание №1 
+
 function parseCount(value) {
     const parsedValue = Number.parseFloat(value);
   
@@ -18,3 +19,39 @@ function parseCount(value) {
   }
 
   // Задание №2
+  
+  class Triangle {
+    constructor(a, b, c) {
+      if (a + b <= c || a + c <= b || b + c <= a) {
+        throw new Error("Треугольник с такими сторонами не существует");
+      }
+      this.a = a;
+      this.b = b;
+      this.c = c;
+    }
+  
+    get perimeter() {
+      return this.a + this.b + this.c;
+    }
+  
+    get area() {
+      const p = this.perimeter / 2;
+      const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+      return parseFloat(area.toFixed(3)); // Округление до 3 знаков после запятой
+    }
+  }
+  
+  function getTriangle(a, b, c) {
+    try {
+      return new Triangle(a, b, c);
+    } catch (error) {
+      return {
+        get area() {
+          return "Ошибка! Треугольник не существует";
+        },
+        get perimeter() {
+          return "Ошибка! Треугольник не существует";
+        },
+      };
+    }
+  }
