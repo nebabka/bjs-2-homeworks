f580eb1eb17e9aa7341189d97c7c2b8e460ba6d1
nebabka <nastyfedinadura@gmail.com>
2025-04-03 18:10:45 +0300

    Задания готовы (7)4

--- 7.async/tests.js
+++ 7.async/tests.js
@@ -1,69 +1,70 @@
 class AlarmClock {
   constructor() {
-      this.alarmCollection = [];
-      this.intervalId = null;
+    this.alarmCollection = [];
+    this.intervalId = null;
   }
 
   addClock(time, callback) {
-      if (time && callback){  
-          
-          if (this.alarmCollection.length > 0) {
-              for (let i = 1; i < this.alarmCollection.length; i=i+1) {
-                  if (this.alarmCollection[i-1].time === time) {
-                  console.warn('Уже присутствует звонок на это же время');
-                  }
-              }
-          }
-          this.alarmCollection.push({time:time, callback:callback, canCall:true});
-          
-      } else {
-          throw new Error ('Отсутствуют обязательные аргументы');
-      }
+    if (!time || !callback) {
+      throw new Error('Отсутствуют обязательные аргументы');
+    }
+
+    if (this.alarmCollection.some(alarm => alarm.time === time)) {
+      console.warn('Уже присутствует звонок на это же время');
+      return;
+    }
+
+    this.alarmCollection.push({
+      time: time,
+      callback: callback,
+      canCall: true
+    });
   }
 
   removeClock(time) {
-      this.alarmCollection = this.alarmCollection.filter((element) => element.time !== time);
+    this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
   }
 
   getCurrentFormattedTime() {
-      let carrentDate = new Date;
-      let hour = carrentDate.getHours();
-      if (hour < 10) {
-          hour = '0'+hour;
-      }
-      let minute = carrentDate.getMinutes()
-      if (minute < 10) {
-          minute = '0'+minute;
-      }
-      return (hour+':'+minute);
+    const now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+
+    hours = String(hours).padStart(2, '0');
+    minutes = String(minutes).padStart(2, '0');
+    return `${hours}:${minutes}`;
   }
 
   start() {
-      if (this.intervalId) {
-          
-      } else {
-          this.intervalId = setInterval(() => {
-              this.alarmCollection.forEach((element) => {
-                  if (element.time === this.getCurrentFormattedTime() && element.canCall) {
-                      element.canCall = false;
-                      element.callback();
-                  }
-              });
-          }, 1000);
-      }
+    if (this.intervalId) {
+      return;
+    }
+
+    this.intervalId = setInterval(() => {
+      this.alarmCollection.forEach(alarm => {
+        if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall) {
+          alarm.canCall = false;
+          alarm.callback();
+        }
+      });
+    }, 1000);
   }
 
   stop() {
-      clearInterval(this.intervalId);
-      this.intervalId = null;
+    clearInterval(this.intervalId);
+    this.intervalId = null;
   }
 
-  resetAllCalls(){
-      this.alarmCollection.forEach((element) => element.canCall = true);
+  resetAllCalls() {
+    this.alarmCollection.forEach(alarm => {
+      alarm.canCall = true;
+    });
   }
 
-  clearAlarms(){
-      this.stop();
+  clearAlarms() {
+    this.stop();
+    setTimeout(() => {
       this.alarmCollection = [];
+    }, 0);
   }
 }
